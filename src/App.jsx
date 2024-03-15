92e300e578700cf8ee4e590129a61790453b95db
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-15 22:10:56 +0600

    fake api added in my site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,89 @@
+import { CgProfile } from "react-icons/cg";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import logo from "./assets/image/logo.jpg";
 import "./App.css";
+import Carts from "./Component/Carts/Carts";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [carts, setCarts] = useState([]);
+  useEffect(() => {
+    fetch("./fakeData.json")
+      .then((res) => res.json())
+      .then((data) => setCarts(data));
+  }, []);
+
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <div className="flex justify-around m-16">
+        <div className="flex space-x-80 justify-between items-center ">
+          <div>
+            <h1 className="text-3xl font-bold">Recipe Calories</h1>
+          </div>
+          <div className="flex space-x-4">
+            <p>Home</p>
+            <p>Recipes</p>
+            <p>About</p>
+            <p>Search</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="flex items-center bg-slate-300 p-2 rounded-full">
+            <div className="">
+              <h3>
+                <HiOutlineMagnifyingGlass />
+              </h3>
+            </div>
+            <div>
+              <input
+                className="bg-slate-300"
+                type="text"
+                name=""
+                id=""
+                placeholder=" Search"
+              />
+            </div>
+          </div>
+          <div className="space-x-5">
+            <h3 className="bg-green-300 p-2 text-3xl rounded-full">
+              <CgProfile />
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-24">
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold mb-2">Our Recipes</h1>
+          <p>
+            Located in the heart of Baltimore’s Arbutus neighborhood, Our
+            Recipes Cafe is a nostalgic blend of coffee house vibes
+          </p>
+        </div>
+      </div>
+      {carts.map((cart) => (
+        <Carts key={carts.id} cart={cart}></Carts>
+      ))}
+    </div>
   );
 }
 
---
+++ src/Component/Carts/Carts.jsx
@@ -0,0 +1,27 @@
+const Carts = ({ cart }) => {
+  console.log(cart);
+  return (
+    <div className="flex">
+      <div className=" mt-16">
+        {/* cart */}
+        <div className="">
+          <img src="" alt="" />
+          <h3>Chicken Caesar Salad</h3>
+          <p>classic italian pasta dish with savory meat sauce.</p>
+          <hr />
+          <h2>Ingredients: 6</h2>
+          <ul>
+            <li>hlw world</li>
+            <li>hlw world</li>
+            <li>hlw world</li>
+          </ul>
+          <hr />
+        </div>
+        {/* site */}
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default Carts;
